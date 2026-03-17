3345a9dbdb497fd2e77010077cff5e597ed3ffab
ColdIV <dev@cldv.org>
2026-03-17 21:58:43 +0100

    remove word types for easier maintenance

--- src/components/FlashCard.vue
+++ src/components/FlashCard.vue
@@ -2,13 +2,11 @@
   <div class="flashcard-container" @click="flip">
     <div class="flashcard" :class="{ flipped, resetting }">
       <div class="flashcard-face flashcard-front">
-        <span class="card-pos">{{ word.pos }}</span>
         <span class="card-word">{{ word.word }}</span>
         <span v-if="word.romanization" class="card-romanization">{{ word.romanization }}</span>
         <span class="card-hint">{{ $t('practice.tapToFlip') }}</span>
       </div>
       <div class="flashcard-face flashcard-back">
-        <span class="card-pos">{{ word.pos }}</span>
         <span class="card-translation">{{ word.translation }}</span>
         <span class="card-original">{{ word.word }}</span>
         <span v-if="word.romanization" class="card-romanization">{{ word.romanization }}</span>
