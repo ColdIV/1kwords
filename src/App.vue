f1b6dd087cef2c73950a77311bdae5ff41a84572
ColdIV <dev@cldv.org>
2026-03-17 22:20:17 +0100

    add learning language text

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <AppHeader />
   <main>
+    <div v-if="currentLangLabel" class="current-language-banner">{{ currentLangLabel }}</div>
     <router-view :key="viewKey" />
   </main>
 </template>
@@ -10,9 +11,15 @@ import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { useSettings } from './composables/useSettings.js'
 import AppHeader from './components/AppHeader.vue'
+import { languages } from './data/languages.js'
 
 const route = useRoute()
 const { settings } = useSettings()
 
 const viewKey = computed(() => `${route.fullPath}-${settings.sourceLang}`)
+
+const currentLangLabel = computed(() => {
+  const lang = languages[settings.sourceLang]
+  return lang ? `Learning ${lang.name}` : ''
+})
 </script>
